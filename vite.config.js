87b711ac4b97ee628cba9a9b08999e457fe007ea
Rodrigo Soares <meuemail.rodrigodev@gmail.com>
2022-07-31 18:06:10 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/rodrigosoadev/Movie_lib.git',
+  base: 'https://github.com/rodrigosoadev/Movie_lib.git/',
   plugins: [react()]
 })
